49ba07bf1c4ee1fedb1b48386cabdcffc1a3a813
JimmyUmanzor <159740825+JimmyUmanzor@users.noreply.github.com>
2024-12-10 23:11:35 -0600

    Cambios con Jason backend y login

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,15 +5,18 @@ const ActividadesRouter = require('./router/ActividadesRouter');
 const AlumnosRouter = require('./router/AlumnosRouter');
 const MaestroRouter = require('./router/MaestroRouter');
 const UsuariosRouter = require('./router/UsuariosRouter');
+const cors = require('cors');
 
 var puerto=5000;
 
+app.use(cors());
 app.use(bodyParser.json())
 
 app.use('/actividades',ActividadesRouter);
 app.use('/alumnos',AlumnosRouter);
 app.use('/maestros',MaestroRouter);
 app.use('/usuarios',UsuariosRouter);
+
 
 app.listen(puerto,()=>{
     console.log('Ejecutando en puerto 5000')
